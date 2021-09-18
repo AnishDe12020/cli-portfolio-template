b41ffc1fae21c626148987400dad2772846b4b4a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-09-18 13:17:32 +0530

    Initial Commit

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ anishde12020
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ anishde12020 --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
